6d898b45a56cdf141d877113aeba365135cc1586
Andres Sainz <andresmsainz@gmail.com>
2021-04-07 06:09:49 -0400

    initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue = 10000;
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("My Value = " + myValue);
+        System.out.println("My Min Value = " + myMinValue);
+        System.out.println("My Max Value = " + myMaxValue);
+    }
+}
