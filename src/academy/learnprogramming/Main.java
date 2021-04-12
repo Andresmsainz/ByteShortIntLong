9d0375fa0a8b2dd4e70a630c27e3e4375f14a576
Andres Sainz <andresmsainz@gmail.com>
2021-04-11 21:45:21 -0400

    Testing different primitive types

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -11,5 +11,22 @@ public class Main {
         System.out.println("My Value = " + myValue);
         System.out.println("My Min Value = " + myMinValue);
         System.out.println("My Max Value = " + myMaxValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("My Min Byte Value = " + myMinByteValue);
+        System.out.println("My Max Byte Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("My Min Short Value = " + myMinShortValue);
+        System.out.println("My Max Short Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("My Long Value = " + myLongValue);
+        System.out.println("My Min Long Value = " + myMinLongValue);
+        System.out.println("My Max Long Value = " + myMaxLongValue);
     }
 }
